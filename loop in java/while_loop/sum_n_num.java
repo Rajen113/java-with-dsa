b713f3e8c216c8741095590c5a1ceeb72bbe4748
rajen <rajendram9516@gmail.com>
2024-12-23 21:46:14 +0530

    write n nutural number sum

--- loop in java/while_loop/sum_n_num.java
+++ loop in java/while_loop/sum_n_num.java
@@ -1,5 +1,22 @@
-package loop in java.while_loop;
+
+
+import java.util.Scanner;
+
 
 public class sum_n_num {
-    
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        int num=1;
+
+        while(n>0){
+            num=num+n;
+            n--;
+
+        }
+
+        System.out.println(num);
+
+    }
 }
